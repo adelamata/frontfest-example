6b1ea56c98b84193d47087f2187c1b5373197b0a
Antonio Ángel de la Mata López <antonio.delamata@atmira.com>
2023-05-30 18:19:25 +0200

    e

--- src/app/modules/cart/cart.component.ts
+++ src/app/modules/cart/cart.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CartProduct } from 'src/app/shared/interfaces/model.interface';
 import { CartService } from './services/cart.service';
-import { Observable } from 'rxjs';
+import { Observable, of, withLatestFrom, map } from 'rxjs';
 
 @Component({
   selector: 'app-cart',
@@ -9,7 +9,20 @@ import { Observable } from 'rxjs';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
-  productsCart$: Observable<Partial<CartProduct>[]> = this.cartService.getCart();
+
+  productsCart$: Observable<
+    Partial<CartProduct>[]
+  > = this.cartService.getCart();
+
+  productsCartTotal$ = this.productsCart$.pipe(
+    map<Partial<CartProduct>[], number>(
+      (productCarts: Partial<CartProduct>[]) =>
+        productCarts.reduce(
+          (acc, product) => acc + product.price! * product.amount!,
+          0
+        )
+    )
+  );
 
   constructor(private readonly cartService: CartService) {}
 
